9a70cd0fd7743e8b46e7ea517450a01515298145
qqrz997 <qqrz997bs@gmail.com>
2024-11-11 18:19:29 +0000

    To file-scoped namespace

--- HitsoundTweaks/HarmonyPatches/AudioTimeSyncController_dspTimeOffset_Patch.cs
+++ HitsoundTweaks/HarmonyPatches/AudioTimeSyncController_dspTimeOffset_Patch.cs
@@ -5,84 +5,83 @@ using System.Reflection;
 using System.Reflection.Emit;
 using UnityEngine;
 
-namespace HitsoundTweaks.HarmonyPatches
+namespace HitsoundTweaks.HarmonyPatches;
+
+/*
+ * For some reason, either the dspTime or the AudioSource time doesn't behave as it should, resulting in the _dspTimeOffset field oscillating between 2 values
+ * This causes hitsound timings to be irregular, which is audible to the player
+ * To fix this, we patch out the _dspTimeOffset update, and reimplement it to be a cumulative average of the target offset calculated each frame
+ * This reliably and consistently gets within a handful of audio samples after a few seconds, which is for all intents and purposes good enough
+ */
+[HarmonyPatch(typeof(AudioTimeSyncController), "Update")]
+internal class AudioTimeSyncController_dspTimeOffset_Patch
 {
-    /*
-     * For some reason, either the dspTime or the AudioSource time doesn't behave as it should, resulting in the _dspTimeOffset field oscillating between 2 values
-     * This causes hitsound timings to be irregular, which is audible to the player
-     * To fix this, we patch out the _dspTimeOffset update, and reimplement it to be a cumulative average of the target offset calculated each frame
-     * This reliably and consistently gets within a handful of audio samples after a few seconds, which is for all intents and purposes good enough
-     */
-    [HarmonyPatch(typeof(AudioTimeSyncController), "Update")]
-    internal class AudioTimeSyncController_dspTimeOffset_Patch
+    static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
     {
-        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        var code = new List<CodeInstruction>(instructions);
+
+        // remove _dspTimeOffset update
+        var skip = true;
+        for (int i = 0; i < code.Count - 8; i++)
         {
-            var code = new List<CodeInstruction>(instructions);
-
-            // remove _dspTimeOffset update
-            var skip = true;
-            for (int i = 0; i < code.Count - 8; i++)
+            if (code[i + 8].opcode == OpCodes.Stfld && (FieldInfo)code[i + 8].operand == AccessTools.Field(typeof(AudioTimeSyncController), "_dspTimeOffset"))
             {
-                if (code[i + 8].opcode == OpCodes.Stfld && (FieldInfo)code[i + 8].operand == AccessTools.Field(typeof(AudioTimeSyncController), "_dspTimeOffset"))
+                // we want the second time this instruction occurs
+                if (skip)
                 {
-                    // we want the second time this instruction occurs
-                    if (skip)
-                    {
-                        skip = false;
-                        continue;
-                    }
-
-                    // don't want to deal with fixing branches, so just replace it with NOPs
-                    for (int j = 0; j < 9; j++)
-                    {
-                        code[i + j].opcode = OpCodes.Nop;
-                    }
-                    break;
+                    skip = false;
+                    continue;
                 }
+
+                // don't want to deal with fixing branches, so just replace it with NOPs
+                for (int j = 0; j < 9; j++)
+                {
+                    code[i + j].opcode = OpCodes.Nop;
+                }
+                break;
             }
-            return code;
+        }
+        return code;
+    }
+
+    // reimplement _dspTimeOffset correction
+    static bool firstCorrectionDone = false;
+    static int averageCount = 1;
+    static double averageOffset = 0.0;
+    static void Postfix(ref double ____dspTimeOffset, AudioSource ____audioSource, float ____timeScale, AudioTimeSyncController.State ____state)
+    {
+        const double maxDiscrepancy = 0.05;
+
+        // the cumulative average trends towards a consistent slightly desynced value, which this offset compensates for
+        // this value works well at both 90 and 60 fps, so I'm assuming it's independent of framerate
+        const double syncOffset = -0.0059;
+
+        if (____state == AudioTimeSyncController.State.Stopped)
+        {
+            firstCorrectionDone = false; // easiest way to reset this flag, Update is reliably called at least a few frames before playback starts
+            return;
         }
 
-        // reimplement _dspTimeOffset correction
-        static bool firstCorrectionDone = false;
-        static int averageCount = 1;
-        static double averageOffset = 0.0;
-        static void Postfix(ref double ____dspTimeOffset, AudioSource ____audioSource, float ____timeScale, AudioTimeSyncController.State ____state)
+        var audioTime = ____audioSource.timeSamples / (double)____audioSource.clip.frequency;
+        var targetOffset = AudioSettings.dspTime - (audioTime / (double)____timeScale);
+
+        if (!firstCorrectionDone || Math.Abs(averageOffset - targetOffset) > maxDiscrepancy)
         {
-            const double maxDiscrepancy = 0.05;
-
-            // the cumulative average trends towards a consistent slightly desynced value, which this offset compensates for
-            // this value works well at both 90 and 60 fps, so I'm assuming it's independent of framerate
-            const double syncOffset = -0.0059;
-
-            if (____state == AudioTimeSyncController.State.Stopped)
-            {
-                firstCorrectionDone = false; // easiest way to reset this flag, Update is reliably called at least a few frames before playback starts
-                return;
-            }
-
-            var audioTime = ____audioSource.timeSamples / (double)____audioSource.clip.frequency;
-            var targetOffset = AudioSettings.dspTime - (audioTime / (double)____timeScale);
-
-            if (!firstCorrectionDone || Math.Abs(averageOffset - targetOffset) > maxDiscrepancy)
-            {
-                averageOffset = targetOffset;
-                averageCount = 1;
-                firstCorrectionDone = true;
-            }
-            else
-            {
-                // lock in value after some time
-                if (averageCount < 10000)
-                {
-                    // update cumulative average
-                    averageOffset = (averageOffset * averageCount + targetOffset) / (averageCount + 1);
-                    averageCount++;
-                }
-            }
-
-            ____dspTimeOffset = averageOffset + syncOffset;
+            averageOffset = targetOffset;
+            averageCount = 1;
+            firstCorrectionDone = true;
         }
+        else
+        {
+            // lock in value after some time
+            if (averageCount < 10000)
+            {
+                // update cumulative average
+                averageOffset = (averageOffset * averageCount + targetOffset) / (averageCount + 1);
+                averageCount++;
+            }
+        }
+
+        ____dspTimeOffset = averageOffset + syncOffset;
     }
 }
--- HitsoundTweaks/Plugin.cs
+++ HitsoundTweaks/Plugin.cs
@@ -8,28 +8,27 @@ using SiraUtil.Zenject;
 using UnityEngine;
 using IPALogger = IPA.Logging.Logger;
 
-namespace HitsoundTweaks
+namespace HitsoundTweaks;
+
+[Plugin(RuntimeOptions.SingleStartInit), NoEnableDisable]
+public class Plugin
 {
-    [Plugin(RuntimeOptions.SingleStartInit), NoEnableDisable]
-    public class Plugin
+    internal static IPALogger Log { get; private set; }
+    internal static Assembly ExecutingAssembly = Assembly.GetExecutingAssembly();
+
+    [Init]
+    /// <summary>
+    /// Called when the plugin is first loaded by IPA (either when the game starts or when the plugin is enabled if it starts disabled).
+    /// [Init] methods that use a Constructor or called before regular methods like InitWithConfig.
+    /// Only use [Init] with one Constructor.
+    /// </summary>
+    public void Init(IPALogger logger, Config config, Zenjector zenjector)
     {
-        internal static IPALogger Log { get; private set; }
-        internal static Assembly ExecutingAssembly = Assembly.GetExecutingAssembly();
+        Log = logger;
+        var pluginConfig = config.Generated<PluginConfig>();   
 
-        [Init]
-        /// <summary>
-        /// Called when the plugin is first loaded by IPA (either when the game starts or when the plugin is enabled if it starts disabled).
-        /// [Init] methods that use a Constructor or called before regular methods like InitWithConfig.
-        /// Only use [Init] with one Constructor.
-        /// </summary>
-        public void Init(IPALogger logger, Config config, Zenjector zenjector)
-        {
-            Log = logger;
-            var pluginConfig = config.Generated<PluginConfig>();   
-
-            zenjector.Install<AppInstaller>(Location.App, pluginConfig);
-            zenjector.Install<MenuInstaller>(Location.Menu);
-            zenjector.Install<PlayerInstaller>(Location.Player);
-        }
+        zenjector.Install<AppInstaller>(Location.App, pluginConfig);
+        zenjector.Install<MenuInstaller>(Location.Menu);
+        zenjector.Install<PlayerInstaller>(Location.Player);
     }
 }
