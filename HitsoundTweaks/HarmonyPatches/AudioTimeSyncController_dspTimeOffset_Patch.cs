39aa46ce565aef8bca52ec3c2f2a2c1a08af18a9
qqrz997 <qqrz997bs@gmail.com>
2024-11-11 20:57:43 +0000

    Fix wrong indentation

--- HitsoundTweaks/HarmonyPatches/AudioTimeSyncController_dspTimeOffset_Patch.cs
+++ HitsoundTweaks/HarmonyPatches/AudioTimeSyncController_dspTimeOffset_Patch.cs
@@ -20,69 +20,68 @@ internal class AudioTimeSyncController_dspTimeOffset_Patch
     {
         var code = new List<CodeInstruction>(instructions);
 
-            // remove _dspTimeOffset update
-            var skip = true;
-            for (int i = 0; i < code.Count - 8; i++)
-            {
-                if (code[i + 8].opcode == OpCodes.Stfld && (FieldInfo)code[i + 8].operand == AccessTools.Field(typeof(AudioTimeSyncController), "_dspTimeOffset"))
-                {
-                    // we want the second time this instruction occurs
-                    if (skip)
-                    {
-                        skip = false;
-                        continue;
-                    }
-
-                    // don't want to deal with fixing branches, so just replace it with NOPs
-                    for (int j = 0; j < 9; j++)
-                    {
-                        code[i + j].opcode = OpCodes.Nop;
-                    }
-                    break;
-                }
-            }
-            return code;
-        }
-
-        // reimplement _dspTimeOffset correction
-        static bool firstCorrectionDone = false;
-        static int averageCount = 1;
-        static double averageOffset = 0.0;
-        static void Postfix(ref double ____dspTimeOffset, AudioSource ____audioSource, float ____timeScale, AudioTimeSyncController.State ____state)
+        // remove _dspTimeOffset update
+        var skip = true;
+        for (int i = 0; i < code.Count - 8; i++)
         {
-            const double maxDiscrepancy = 0.05;
-
-            // the cumulative average trends towards a consistent slightly desynced value, which this offset compensates for
-            // this value works well at both 90 and 60 fps, so I'm assuming it's independent of framerate
-            const double syncOffset = -0.0059;
-
-            if (____state == AudioTimeSyncController.State.Stopped)
+            if (code[i + 8].opcode == OpCodes.Stfld && (FieldInfo)code[i + 8].operand == AccessTools.Field(typeof(AudioTimeSyncController), "_dspTimeOffset"))
             {
-                firstCorrectionDone = false; // easiest way to reset this flag, Update is reliably called at least a few frames before playback starts
-                return;
-            }
-
-            var audioTime = ____audioSource.timeSamples / (double)____audioSource.clip.frequency;
-            var targetOffset = AudioSettings.dspTime - (audioTime / (double)____timeScale);
-
-            if (!firstCorrectionDone || Math.Abs(averageOffset - targetOffset) > maxDiscrepancy)
-            {
-                averageOffset = targetOffset;
-                averageCount = 1;
-                firstCorrectionDone = true;
-            }
-            else
-            {
-                // lock in value after some time
-                if (averageCount < 10000)
+                // we want the second time this instruction occurs
+                if (skip)
                 {
-                    // update cumulative average
-                    averageOffset = (averageOffset * averageCount + targetOffset) / (averageCount + 1);
-                    averageCount++;
+                    skip = false;
+                    continue;
                 }
-            }
 
-            ____dspTimeOffset = averageOffset + syncOffset;
+                // don't want to deal with fixing branches, so just replace it with NOPs
+                for (int j = 0; j < 9; j++)
+                {
+                    code[i + j].opcode = OpCodes.Nop;
+                }
+                break;
+            }
         }
+        return code;
+    }
+
+    // reimplement _dspTimeOffset correction
+    static bool firstCorrectionDone = false;
+    static int averageCount = 1;
+    static double averageOffset = 0.0;
+    static void Postfix(ref double ____dspTimeOffset, AudioSource ____audioSource, float ____timeScale, AudioTimeSyncController.State ____state)
+    {
+        const double maxDiscrepancy = 0.05;
+
+        // the cumulative average trends towards a consistent slightly desynced value, which this offset compensates for
+        // this value works well at both 90 and 60 fps, so I'm assuming it's independent of framerate
+        const double syncOffset = -0.0059;
+
+        if (____state == AudioTimeSyncController.State.Stopped)
+        {
+            firstCorrectionDone = false; // easiest way to reset this flag, Update is reliably called at least a few frames before playback starts
+            return;
+        }
+
+        var audioTime = ____audioSource.timeSamples / (double)____audioSource.clip.frequency;
+        var targetOffset = AudioSettings.dspTime - (audioTime / (double)____timeScale);
+
+        if (!firstCorrectionDone || Math.Abs(averageOffset - targetOffset) > maxDiscrepancy)
+        {
+            averageOffset = targetOffset;
+            averageCount = 1;
+            firstCorrectionDone = true;
+        }
+        else
+        {
+            // lock in value after some time
+            if (averageCount < 10000)
+            {
+                // update cumulative average
+                averageOffset = (averageOffset * averageCount + targetOffset) / (averageCount + 1);
+                averageCount++;
+            }
+        }
+
+        ____dspTimeOffset = averageOffset + syncOffset;
     }
 }
--- HitsoundTweaks/HarmonyPatches/Hitsound_Reliability_Patches.cs
+++ HitsoundTweaks/HarmonyPatches/Hitsound_Reliability_Patches.cs
@@ -19,60 +19,59 @@ internal class Hitsound_Reliability_Patches
     {
         var code = new List<CodeInstruction>(instructions);
 
-            // disable setting audio source priority
-            for (int i = 0; i < code.Count - 3; i++)
-            {
-                if (code[i + 3].opcode == OpCodes.Callvirt && (MethodInfo)code[i + 3].operand == AccessTools.PropertySetter(typeof(AudioSource), nameof(AudioSource.priority)))
-                {
-                    // don't want to deal with fixing branches, so just replace it with NOPs
-                    for (int j = 0; j < 4; j++)
-                    {
-                        code[i + j].opcode = OpCodes.Nop;
-                    }
-                    break;
-                }
-            }
-
-            return code;
-        }
-
-        // retry scheduling playback just ahead of time
-        static void Prefix(AudioSource ____audioSource, double ____startDSPTime, ref bool ____isPlaying)
+        // disable setting audio source priority
+        for (int i = 0; i < code.Count - 3; i++)
         {
-            const double retryAheadTime = 0.05;
-            if (!____isPlaying && ____startDSPTime - AudioSettings.dspTime < retryAheadTime)
+            if (code[i + 3].opcode == OpCodes.Callvirt && (MethodInfo)code[i + 3].operand == AccessTools.PropertySetter(typeof(AudioSource), nameof(AudioSource.priority)))
             {
-                // this is extremely hacky, absolutely terrible, and I shouldn't be doing this
-                // but I have to keep track of state somewhere, and this seems to be unused
-                // I'm sorry
-                ____isPlaying = true;
-
-                if (!____audioSource.isPlaying)
+                // don't want to deal with fixing branches, so just replace it with NOPs
+                for (int j = 0; j < 4; j++)
                 {
-                    ____audioSource.Stop();
-                    ____audioSource.timeSamples = 0;
-                    ____audioSource.PlayScheduled(____startDSPTime);
+                    code[i + j].opcode = OpCodes.Nop;
                 }
+                break;
             }
         }
 
-        static void Postfix(bool ____noteWasCut, AudioSource ____audioSource, bool ____goodCut, double ____startDSPTime, double ____aheadTime)
+        return code;
+    }
+
+    // retry scheduling playback just ahead of time
+    static void Prefix(AudioSource ____audioSource, double ____startDSPTime, ref bool ____isPlaying)
+    {
+        const double retryAheadTime = 0.05;
+        if (!____isPlaying && ____startDSPTime - AudioSettings.dspTime < retryAheadTime)
         {
-            // set priority based on time relative to note time
-            var dspTime = AudioSettings.dspTime;
-            var noteTime = ____startDSPTime + ____aheadTime;
-            if (!(____noteWasCut && !____goodCut)) // note hasn't been bad cut
+            // this is extremely hacky, absolutely terrible, and I shouldn't be doing this
+            // but I have to keep track of state somewhere, and this seems to be unused
+            // I'm sorry
+            ____isPlaying = true;
+
+            if (!____audioSource.isPlaying)
             {
-                if (dspTime - noteTime > 0)
-                {
-                    const float priorityFalloff = 384;
-                    ____audioSource.priority = Mathf.Clamp(32 + Mathf.RoundToInt((float)(dspTime - noteTime) * priorityFalloff), 32, 127);
-                }
-                else
-                {
-                    const float priorityRampup = 192;
-                    ____audioSource.priority = Mathf.Clamp(32 + Mathf.RoundToInt((float)(noteTime - dspTime) * priorityRampup), 32, 128);
-                }
+                ____audioSource.Stop();
+                ____audioSource.timeSamples = 0;
+                ____audioSource.PlayScheduled(____startDSPTime);
+            }
+        }
+    }
+
+    static void Postfix(bool ____noteWasCut, AudioSource ____audioSource, bool ____goodCut, double ____startDSPTime, double ____aheadTime)
+    {
+        // set priority based on time relative to note time
+        var dspTime = AudioSettings.dspTime;
+        var noteTime = ____startDSPTime + ____aheadTime;
+        if (!(____noteWasCut && !____goodCut)) // note hasn't been bad cut
+        {
+            if (dspTime - noteTime > 0)
+            {
+                const float priorityFalloff = 384;
+                ____audioSource.priority = Mathf.Clamp(32 + Mathf.RoundToInt((float)(dspTime - noteTime) * priorityFalloff), 32, 127);
+            }
+            else
+            {
+                const float priorityRampup = 192;
+                ____audioSource.priority = Mathf.Clamp(32 + Mathf.RoundToInt((float)(noteTime - dspTime) * priorityRampup), 32, 128);
             }
         }
     }
