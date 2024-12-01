7f5fe4babc4d8c08073dfb6bb7045696baf941dc
qqrz997 <156676353+qqrz997@users.noreply.github.com>
2024-12-01 10:44:46 +0000

    Remove BSML attributes from PluginConfig

--- HitsoundTweaks/Configuration/PluginConfig.cs
+++ HitsoundTweaks/Configuration/PluginConfig.cs
@@ -1,5 +1,4 @@
-﻿using BeatSaberMarkupLanguage.Attributes;
-using IPA.Config.Stores;
+﻿using IPA.Config.Stores;
 using System.Runtime.CompilerServices;
 
 [assembly: InternalsVisibleTo(GeneratedStore.AssemblyVisibilityTarget)]
@@ -7,24 +6,11 @@ namespace HitsoundTweaks.Configuration;
 
 internal class PluginConfig
 {
-    [UIValue("ignore-saber-speed")]
     public virtual bool IgnoreSaberSpeed { get; set; } = false;
-
-    [UIValue("static-sound-pos")]
     public virtual bool StaticSoundPos { get; set; } = false;
-
-    [UIValue("enable-spatialization")]
     public virtual bool EnableSpatialization { get; set; } = true;
-
-    [UIValue("random-pitch-min")]
     public virtual float RandomPitchMin { get; set; } = 0.9f;
-
-    [UIValue("random-pitch-max")]
     public virtual float RandomPitchMax { get; set; } = 1.2f;
-
-    [UIValue("enable-chain-element-hitsounds")]
     public virtual bool EnableChainElementHitsounds { get; set; } = false;
-
-    [UIValue("chain-element-volume-multiplier")]
     public virtual float ChainElementVolumeMultiplier { get; set; } = 0.8f;
 }
