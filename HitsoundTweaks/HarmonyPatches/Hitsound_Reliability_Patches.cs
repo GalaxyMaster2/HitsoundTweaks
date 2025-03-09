b5beb5147137bb222b628252f3739930365b1ebd
GalaxyMaster2 <10052298+GalaxyMaster2@users.noreply.github.com>
2025-03-09 14:05:14 +0100

    disable playback schedule on hitsound init
    maps with VNJS can spawn notes many seconds ahead of time
    because of this it no longer makes sense to schedule hitsounds on init
    instead we only schedule them just ahead of time

--- HitsoundTweaks/HarmonyPatches/Hitsound_Reliability_Patches.cs
+++ HitsoundTweaks/HarmonyPatches/Hitsound_Reliability_Patches.cs
@@ -9,15 +9,38 @@ namespace HitsoundTweaks.HarmonyPatches;
 
 /*
  * This makes 2 changes to make hitsounds play more reliably
- * 1. Scheduling playback silently fails when there are no virtual voices available. To alleviate this, we retry scheduling playback just ahead of time.
+ * 1. Scheduling playback silently fails when there are no virtual voices available. To alleviate this, we schedule playback just ahead of time instead of at note spawn.
  * 2. AudioSource priorities are not managed very smoothly, which gives Unity little time to sort things out, leading to hitsounds cutting out, not playing, etc.
  *    This reworks priority assignment to smoothly scale priorities based on time relative to the note it's meant for, which greatly improves reliability
  */
 internal class Hitsound_Reliability_Patches : IAffinity
 {
     [AffinityTranspiler]
+    [AffinityPatch(typeof(NoteCutSoundEffect), nameof(NoteCutSoundEffect.Init))]
+    private IEnumerable<CodeInstruction> InitTranspiler(IEnumerable<CodeInstruction> instructions)
+    {
+        var code = new List<CodeInstruction>(instructions);
+
+        // disable hitsound schedule on init
+        for (int i = 0; i < code.Count - 4; i++)
+        {
+            if (code[i + 4].opcode == OpCodes.Callvirt && (MethodInfo)code[i + 4].operand == AccessTools.Method(typeof(AudioSource), nameof(AudioSource.PlayScheduled)))
+            {
+                // don't want to deal with fixing branches, so just replace it with NOPs
+                for (int j = 0; j < 5; j++)
+                {
+                    code[i + j].opcode = OpCodes.Nop;
+                }
+                break;
+            }
+        }
+
+        return code;
+    }
+
+    [AffinityTranspiler]
     [AffinityPatch(typeof(NoteCutSoundEffect), nameof(NoteCutSoundEffect.OnLateUpdate))]
-    private IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+    private IEnumerable<CodeInstruction> LateUpdateTranspiler(IEnumerable<CodeInstruction> instructions)
     {
         var code = new List<CodeInstruction>(instructions);
 
@@ -40,11 +63,11 @@ internal class Hitsound_Reliability_Patches : IAffinity
 
     [AffinityPrefix]
     [AffinityPatch(typeof(NoteCutSoundEffect), nameof(NoteCutSoundEffect.OnLateUpdate))]
-    // retry scheduling playback just ahead of time
+    // try scheduling playback just ahead of time
     private void Prefix(AudioSource ____audioSource, double ____startDSPTime, ref bool ____isPlaying)
     {
-        const double retryAheadTime = 0.05;
-        if (!____isPlaying && ____startDSPTime - AudioSettings.dspTime < retryAheadTime)
+        const double scheduleAheadTime = 0.05;
+        if (!____isPlaying && ____startDSPTime - AudioSettings.dspTime < scheduleAheadTime)
         {
             // this is extremely hacky, absolutely terrible, and I shouldn't be doing this
             // but I have to keep track of state somewhere, and this seems to be unused
@@ -53,7 +76,6 @@ internal class Hitsound_Reliability_Patches : IAffinity
 
             if (!____audioSource.isPlaying)
             {
-                ____audioSource.Stop();
                 ____audioSource.timeSamples = 0;
                 ____audioSource.PlayScheduled(____startDSPTime);
             }
